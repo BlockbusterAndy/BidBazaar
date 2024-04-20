2508fa74ec9e63dc0b5b4b32502789860ae25131
BlockbusterAndy <124876077+BlockbusterAndy@users.noreply.github.com>
2024-04-21 00:50:56 +0530

    Update ALLOWED_HOSTS in commerce/settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.69.221', 'localhost', '127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['192.168.69.221', 'localhost', '127.0.0.1','.vercel.app','.now.sh']
 
 WSGI_APPLICATION = 'api.wsgi.app'
 
