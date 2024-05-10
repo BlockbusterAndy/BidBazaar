59bec4820a12f737af28dd063dc3eb2064247f05
Aniket Jadhav <124876077+BlockbusterAndy@users.noreply.github.com>
2024-05-11 02:15:33 +0530

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -146,6 +146,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+#Change with your own email and app specific password
 EMAIL_HOST_USER = 'bidbazaar2024@gmail.com'
 EMAIL_HOST_PASSWORD = 'veur iymr rtdj twki'
 
